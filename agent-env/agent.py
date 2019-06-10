a60646e9b23558fff079b4dbb375ccc45aba2da6
ChoiSeonMun <ch002717@gmail.com>
2019-06-10 22:35:27 +0900

    Distribute lecture room & Add a new constraint & preference for room

--- agent-env/agent.py
+++ agent-env/agent.py
@@ -4,76 +4,93 @@ from room_constraint import *
 from Day import Day
 from LectureRoom import LectureRoom
 from csv_process import writeCsvFile
+import random
 
 dayDomain = [ Day(i) for i in range(5) ]
-roomDomain = lectureRoomsForSorting
 timeDomain = [ 9 + i for i in range(8) ]
 
-def processTheorySubject(subject):
-    # 공학설계 및 졸업설계 과목은 배치하지 않는다.
-    if isDesignSubject(subject):
-        return True
+# 담헌 실학관을 제해준다.
+theoryRoomDomain = roomsForTheory[:-1]
+practiceRoomDomain = roomsForPractice
 
-    for (cap, room) in roomDomain:
-        # 강의실이 수용할 수 있는가?
-        if hasMoreCapacity(subject, room) == False:
+# 강의시간을 배정한다.
+# @param
+# subject : 과목
+# room : 강의실
+# @return
+# 배정에 성공한 경우 True를 반환한다.
+def assignTime(subject, room):
+    for day in dayDomain:
+        # 실습 과목일 경우, 이론 수업과 다른 날짜인가?
+        if subject.isPractice and isSameAsTheory(subject, day):
             continue
-        for day in dayDomain:
-            for time in timeDomain:
-                # 점심시간인가?
-                if isNoon(time):
-                    continue
 
-                # 해당 시간에 강의실을 사용할 수 있는가?
-                if canAssign(room, day, time, subject):
-                    lectureRoom = lectureRooms[room]
-                    lectureRoom.assignSubject(day, time, subject)
-                    subject.setStartTime(day, time)
-                    return True
+        for time in timeDomain:
+            # 점심시간인가?
+            if isNoon(time):
+                continue
+
+            # 해당 시간에 강의실을 사용할 수 있는가?
+            if canAssign(room, day, time, subject):
+                room.assignSubject(day, time, subject)
+                subject.setStartTime(day, time)
+                return True
     return False
 
-def processPracticeSubject(subject):
-    for (cap, room) in roomDomain:
-        # 실습실인가?
-        if not canPractice(room):
-            continue
+# 강의실을 배정한다.
+# @param
+# domain : 사용 가능한 강의실
+# subject : 넣을 과목
+# isPractice : 실습 과목을 배정하는 것인가?
+# @return
+# 배정에 성공하면 True를 반환한다.
+def assignRoom(domain, subject):
+    if subject.lectureRoom:
+        return assignTime(subject, subject.lectureRoom)
+
+    for i in range(len(domain) * 2):
+        room = random.choice(domain)
         
         # 강의실이 수용할 수 있는가?
         if hasMoreCapacity(subject, room) == False:
             continue
 
-        for day in dayDomain:
-            # 이론 수업과 다른 날짜인가?
-            if isSameAsTheory(subject, day):
-                continue
-
-            for time in timeDomain:
-                # 점심시간인가?
-                if isNoon(time):
-                    continue
-
-                # 해당 시간에 강의실을 사용할 수 있는가?
-                if canAssign(room, day, time, subject):
-                    lectureRoom = lectureRooms[room]
-                    lectureRoom.assignSubject(day, time, subject)
-                    subject.setStartTime(day, time)
-                    return True
+        # 시간을 배정한다.
+        if assignTime(subject, room):
+            return True
     return False
+
+# 해당 과목에 강의 시간과 강의실을 배정한다.
+# @param
+# subject : 과목
+# @return
+# 배정이 성공했다면 True를 반환한다.
+def processSubject(subject):
+    # 이론 과목인 경우
+    if not subject.isPractice:
+        # 공학설계 및 졸업설계 과목은 배치하지 않는다.
+        if isDesignSubject(subject):
+            return True
+
+        # 선호 1번
+        if assignRoom(theoryRoomDomain, subject):
+            return True
+    
+    return assignRoom(practiceRoomDomain, subject)
 
 # 이론 과목에 대해서 먼저 배치한다.
 for (name, subject) in theorySubjects.items():
-    if not processTheorySubject(subject):
+    if not processSubject(subject):
         subject.printDetail()
 print("------------Theory Done---------------")
 
 # 그다음 실습 과목에 대해서 배치한다.
 for (name, subject) in practiceSubjects.items():
-    if not processPracticeSubject(subject):
+    if not processSubject(subject):
         subject.printDetail()
 print("------------Practice Done---------------")
 
-#for (name, subject) in theorySubjects.items():
-#    subject.printDetail()
 
 lines = Subject.toListForCsv(theorySubjects, practiceSubjects)
-writeCsvFile("Result1.csv", lines)
+#writeCsvFile("Result1.csv", lines)
+writeCsvFile("Result2.csv", lines)
--- agent-env/room_constraint.py
+++ agent-env/room_constraint.py
@@ -1,47 +1,40 @@
 from Subject import Subject
 from LectureRoom import LectureRoom
-from environment import lectureRooms
 from Day import Day
 
 """
 강의실에 대한 제약사항
 1. 강의실의 정원은 수강정원보다 같거나 커야 한다.
-2. 이론/실습 과목에 따라 적절한 강의실이 배정되어야 한다.
+2. 수업에 따라 적절한 강의실이 배정되어야 한다.
 3. 공학설계, 졸업설계의 강의실은 배정되지 않는다.
 4. 각 강의실은 동일한 시간대의 과목에 대해서 중복 배치될 수 없다.
+5. 강의실은 여러 과목에 분산되어 배치되어야 한다.
+6. 산업체취·창업특강, 정보보호개론 외에는 담헌실학관을 사용하지 않는다.
+
+선호
+1. 이론 수업은 실습 강의실보단 이론 강의실을 택한다.
 """
 
 # 강의실이 해당 과목의 인원을 수용할 수 있는지 검사한다.
 # @pamam
 # subject : 과목
-# roomName : 강의실 이름
+# room : 강의실
 # @return
 # 수용할 수 있으면 True를 반환한다.
-def hasMoreCapacity(subject, roomName):
-    room = lectureRooms[roomName]
+def hasMoreCapacity(subject, room):
     return room.capacity >= subject.capacity
-
-# 강의실이 실습실인지 검사한다.
-# @param
-# roomName : 강의실 이름
-# @return
-# 해당 강의실이 실습실이면 True를 반환한다.
-def canPractice(roomName):
-    room = lectureRooms[roomName]
-    return room.canPractice
 
 # 특정 시간에 강의실을 이용할 수 있는지 검사한다.
 # @param
-# roomName : 강의실 이름
+# room : 강의실
 # day : 날짜
 # start : 시작시간
-# time : 강의시간
-def canAssign(roomName, day, start, subject):
+# subject : 과목
+def canAssign(room, day, start, subject):
     if start == 12:
         return False
 
     time = subject.time
-    room = lectureRooms[roomName]
     timeTable = room.getAvailableTimeTable(day)
     for i in range(time):
         if start + i not in timeTable:
@@ -50,22 +43,17 @@ def canAssign(roomName, day, start, subject):
 
 if __name__ == "__main__":
     tempRoom = LectureRoom("테스트 강의실", 104, 25, False)
-    lectureRooms["테스트 강의실"] = tempRoom
     tempSubj = Subject("테스트", 2, True, 1, 40, None, "수14")
 
     print("hasMoreCapacity")
     print("--------------------")
-    print("Expected False, Real:", hasMoreCapacity(tempSubj, "테스트 강의실"))
-    
-    print("canPractice")
-    print("--------------------")
-    print("Expected False, Real:", canPractice("테스트 강의실"))
+    print("Expected False, Real:", hasMoreCapacity(tempSubj, tempRoom))
 
     print("canAssign")
     print("--------------------")
     start = 9
     tempSubj2 = Subject("테스트2", 2, False, 1, 25, None, None)
     tempRoom.assignSubject(Day.TUE, start, tempSubj2)
-    print("Expected False, Real:", canAssign("테스트 강의실", Day.TUE, 10, 2))
-    print("Expected True, Real:", canAssign("테스트 강의실", Day.TUE, 11, 2))
+    print("Expected False, Real:", canAssign(tempRoom, Day.TUE, 10, 2))
+    print("Expected True, Real:", canAssign(tempRoom, Day.TUE, 11, 2))
 
